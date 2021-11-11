ad352bfbbeb4cee237a0c55c78e0eae24b3df24d
Jack <mrzzcn@gmail.com>
2021-11-11 11:11:09 +0800

    add option: timeout

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ const defaultOptions = {
   //# another feature creep
   // tribute to Netflix Server Side Only React https://twitter.com/NetflixUIE/status/923374215041912833
   // but this will also remove code which registers service worker
-  removeScriptTags: false
+  removeScriptTags: false,
+  timeout: 0
 };
 
 /**
